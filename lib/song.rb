1d68e7c0e9277fbb5daf4d9ded68afc15725ea49
Reno Sorensen <renojames19@gmail.com>
2018-05-05 01:40:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
 
   def self.artist_count
     artist_count = {}
-    @@artists do | artist |
+    @@artists.each do | artist |
       if !artist_count.include?(artist)
         artist_count[artist] = 1
       else
